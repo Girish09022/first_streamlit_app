f64b8fc271378f66d69f837aedfc75f2c89c9d32
Girish Lakhani <108714352+Girish09022@users.noreply.github.com>
2022-07-05 11:24:19 +0530

    Update streamlit_app.py
    New msg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My ParentsNew Healthy Diner')
+
+streamlit.hearder('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
